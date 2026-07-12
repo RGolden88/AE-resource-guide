39eaf2359a052e22929e59c07b6f85ed5e015f87
RGolden88 <robert.golden75@yahoo.com>
2026-07-11 19:21:00 -0500

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -125,6 +125,18 @@ calculateRent.addEventListener("click", function () {
 
   const totalParcels = common + rare + epic + legendary;
 
+let commonPercent = 0;
+let rarePercent = 0;
+let epicPercent = 0;
+let legendaryPercent = 0;
+
+if (totalParcels > 0) {
+  commonPercent = (common / totalParcels) * 100;
+  rarePercent = (rare / totalParcels) * 100;
+  epicPercent = (epic / totalParcels) * 100;
+  legendaryPercent = (legendary / totalParcels) * 100;
+}
+
   /*
     LPU logic:
     LPUs make a parcel earn like legendary.
@@ -318,4 +330,48 @@ calculateGoal.addEventListener("click", function () {
 
   document.getElementById("additionalParcelsNeeded").textContent =
     additionalParcels;
+});
+
+const calculateChecklist = document.getElementById("calculateChecklist");
+const clearChecklist = document.getElementById("clearChecklist");
+
+calculateChecklist.addEventListener("click", function () {
+  const doneChallenges = document.querySelectorAll(".done-challenge");
+  const plannedChallenges = document.querySelectorAll(".planned-challenge");
+
+  let completedPoints = 0;
+  let plannedPoints = 0;
+
+  doneChallenges.forEach(function (challenge) {
+    if (challenge.checked) {
+      completedPoints += Number(challenge.dataset.points);
+    }
+  });
+
+  plannedChallenges.forEach(function (challenge) {
+    if (challenge.checked) {
+      plannedPoints += Number(challenge.dataset.points);
+    }
+  });
+
+  const projectedPoints = completedPoints + plannedPoints;
+  const pointsNeeded = Math.max(1600 - projectedPoints, 0);
+
+  document.getElementById("completedPoints").textContent = completedPoints;
+  document.getElementById("plannedPoints").textContent = plannedPoints;
+  document.getElementById("projectedPoints").textContent = projectedPoints;
+  document.getElementById("neededChecklistPoints").textContent = pointsNeeded;
+});
+
+clearChecklist.addEventListener("click", function () {
+  const boxes = document.querySelectorAll("input[type='checkbox']");
+
+  boxes.forEach(function (box) {
+    box.checked = false;
+  });
+
+  document.getElementById("completedPoints").textContent = 0;
+  document.getElementById("plannedPoints").textContent = 0;
+  document.getElementById("projectedPoints").textContent = 0;
+  document.getElementById("neededChecklistPoints").textContent = 1600;
 });
